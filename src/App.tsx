0b507320e230a871a6f9ac1d85825d41e2205f3e
WeisgerberEstelle <89032759+WeisgerberEstelle@users.noreply.github.com>
2025-08-13 15:29:24 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <main>
+        <h1 className="text-3xl">Welcome to my 3D portfolio</h1>
+        <div>App</div>   
+    </main>  
+  )
+};
+
+App.propTypes = {};
+
+export default App;
