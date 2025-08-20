7ffa11897f7e73414e477804f19cd8c995126c3f
WeisgerberEstelle <89032759+WeisgerberEstelle@users.noreply.github.com>
2025-08-20 10:48:20 +0200

    feat: add public folder

---
+++ src/constants/index.ts
@@ -0,0 +1,12 @@
+const words = [
+    { text: 'Ideas', imgPath: '/images/ideas.svg' },
+    { text: 'Texts', imgPath: '/images/concepts.svg' },
+    { text: 'Designs', imgPath: '/images/designs.svg' },
+    { text: 'Images', imgPath: '/images/code.svg' },
+    { text: 'Ideas', imgPath: '/images/ideas.svg' },
+    { text: 'Texts', imgPath: '/images/concepts.svg' },
+    { text: 'Designs', imgPath: '/images/designs.svg' },
+    { text: 'Images', imgPath: '/images/code.svg' }
+];
+
+export { words };
